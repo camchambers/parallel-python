eb2f10e18b5dc152f4b1e6d6cfe3677430aa5590
Cameron Chambers <cameronchambers@trentu.ca>
2019-07-04 14:43:27 -0400

    Updated range_count function with default parameter values

--- sequential.py
+++ sequential.py
@@ -4,31 +4,28 @@
 import numpy as np
 import time
 
-# Track the start time of the program
-startTime = time.time()
-
 # Initialize random number generator with a seed value
 np.random.RandomState(999)
 
-
-# Generate an array of random number between 0 and 10 with a shape of
-# 10 by 100000
-numberArray = np.random.randint(0, 10, size=[10000000, 10])
+# Generate an array of random number between 0 and 10
+numberArray = np.random.randint(0, 10, size=[10000, 10000])
 data = numberArray.tolist()
 
 # A function that counts how many elements fall between a range of values
 # given a one-dimensional array
-def range_count(row, minimum, maximum):
+def range_count(row, minimum=2, maximum=5):
     count = 0
     for n in row:
         if minimum <= n <= maximum:
             count = count + 1
     return count
 
-
 # An array to store how many values fall between each record in our array
 # of values
 results = []
+
+# Track the start time of the program
+startTime = time.time()
 
 # Iterate through each row in the 2D array of values and determine how many times
 # each of the values fall between our range of specified values
