b4f4ca69f34ea52c0ba217bccdc5cb0a52a9de9d
Cameron Chambers <cameron0chambers@gmail.com>
2020-02-23 07:52:42 -0500

    Moved the range_count function to the beggining of the script for the paralle.py program

--- parallel.py
+++ parallel.py
@@ -5,19 +5,22 @@ import numpy as np
 import time
 import multiprocessing as mp
 
-# Initialize random number generator with a seed value
-np.random.RandomState(999)
-
-# Generate an array of random number between 0 and 10
-numberArray = np.random.randint(0, 10, size=[10000, 10000])
-data = numberArray.tolist()
-
 def range_count(row, minimum=2, maximum=5):
     count = 0
     for n in row:
         if minimum <= n <= maximum:
             count = count + 1
     return count
+
+# Display the number of processors available on the current machine
+print("Number of processors: ", mp.cpu_count())
+
+# Initialize random number generator with a seed value
+np.random.RandomState(999)
+
+# Generate an array of random number between 0 and 10
+numberArray = np.random.randint(0, 10, size=[10000, 10000])
+data = numberArray.tolist()
 
 # Track the start time of the program
 startTime = time.time()
@@ -27,13 +30,10 @@ pool = mp.Pool(mp.cpu_count())
 
 results = pool.map(range_count, [row for row in data])
 
-# Step 3: Don't forget to close
+# Close the pool of processors
 pool.close()    
 
 # Determine the execution time of the program
 totalTime = time.time() - startTime
 
 print("Parallel execution completed in %s seconds." % (totalTime))
-
-# Print results from the first 5 rows
-print(results[:5])
