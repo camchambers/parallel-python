1808dd59accb1724ee44c29841b418d1d039f91b
Cameron Chambers <cameron0chambers@gmail.com>
2020-05-12 21:01:56 -0400

    Refactored the sequential program to use a single processor

--- sequential.py
+++ sequential.py
@@ -1,40 +1,40 @@
 # A simple program that iterates through a two-dimensional array and determines how
 # many values within each row fall within a user-defined range.
-# This example serves as a baseline for demonstrating the speedup 
-# of a a parallel version.
+# This example is a sequential version that explicitly uses 1 processor.
 
 import numpy as np
 import time
+import multiprocessing as mp
 
 # A function that counts how many elements fall between a range of values
 # given a one-dimensional array
-def range_count(row, minimum, maximum):
+def range_count(row, minimum=2, maximum=5):
     count = 0
     for n in row:
         if minimum <= n <= maximum:
             count = count + 1
     return count
 
+# Display the number of processors available on the current machine
+print("Using {} CPU(s)".format(1))
+
 # Initialize random number generator with a seed value
 np.random.RandomState(999)
 
-# Generate a two-dimensional 10,000 x 10,000 array of random
-# numbers falling between 0 and 10
-numberArray = np.random.randint(0, 10, size=[10000, 10000])
+# Generate an array of random number between 0 and 10
+numberArray = np.random.randint(0, 10, size=[15000, 15000])
 data = numberArray.tolist()
-
-# An array to store how many values fall between each record in our array
-# of values
-results = []
 
 # Track the start time of the program
 startTime = time.time()
 
-# Iterate through each row in the two-dimensional array of results 
-# and determine how many times each of the values fall between
-# the user-defined range of values
-for row in data:
-    results.append(range_count(row, minimum=2, maximum=5))
+# Create a pool of processors
+pool = mp.Pool(1)
+
+results = pool.map(range_count, [row for row in data])
+
+# Close the pool of processors
+pool.close()    
 
 # Determine the execution time of the program
 totalTime = time.time() - startTime
